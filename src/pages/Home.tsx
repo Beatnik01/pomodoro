9124e29657d1f0b36b63e37d41ea1da20e030f74
Gihoon Lim <knv429@naver.com>
2024-03-14 16:10:18 +0900

    타이머 시작, 정지버튼 round, goal 카운트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -87,35 +88,89 @@ const Count = styled.div`
 `;
 
 export default function Home() {
+  const [isStarted, setIsStarted] = useState(false);
+  const [isRested, setIsRested] = useState(false);
+  const [minutes, setMinutes] = useState(25);
+  const [second, setSecond] = useState(0);
+  const [round, setRound] = useState(0);
+  const [goal, setGoal] = useState(0);
+
+  const onStart = () => {
+    setIsStarted((prev) => !prev);
+  };
+  useEffect(() => {
+    if (minutes < 0) {
+      setRound((prev) => prev + 1);
+      setIsRested(true);
+    }
+
+    if (second < 0) {
+      setMinutes((prev) => prev - 1);
+      setSecond(59);
+    }
+
+    if (round === 4) {
+      setRound(0);
+      setGoal((prev) => prev + 1);
+    }
+
+    if (isStarted) {
+      const timer = setInterval(() => {
+        setSecond((prev) => prev - 1);
+      }, 1000);
+      return () => clearInterval(timer);
+    }
+
+    if (isRested) {
+      round === 4 ? setMinutes(30) : setMinutes(5);
+    }
+  });
   return (
     <Container>
       <Title>Pomodoro</Title>
       <TimerContainer>
-        <Time>10</Time>
+        <Time>{String(minutes).padStart(2, "0")}</Time>
         <Colon>:</Colon>
-        <Time>00</Time>
+        <Time>{String(second).padStart(2, "0")}</Time>
       </TimerContainer>
-      <Button>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            fillRule="evenodd"
-            d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </Button>
+      {isStarted ? (
+        <Button onClick={onStart}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Button>
+      ) : (
+        <Button onClick={onStart}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Button>
+      )}
       <CountContainer>
         <Count>
-          <span>0/4</span>
+          <span>{round}/4</span>
           <span>ROUND</span>
         </Count>
         <Count>
-          <span>0/12</span>
+          <span>{goal}/12</span>
           <span>GOAL</span>
         </Count>
       </CountContainer>
